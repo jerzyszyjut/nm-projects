17e25176009ef95e924d16726e8da124507e28b3
jerzyszyjut <jerzy.szyjut@outlook.com>
2024-04-20 19:37:50 +0200

    Improve projects file structure

--- matrices/main.py
+++ matrices/main.py
@@ -3,6 +3,13 @@ from matrix_generator import UserMatrix, VectorB
 from solvers import GaussSolver, JacobiSolver, GaussSeidelSolver, LUFactorizationSolver
 import matplotlib.pyplot as plt
 import pickle
+import os
+
+if not os.path.exists("snapshots"):
+    os.makedirs("snapshots")
+if not os.path.exists("images"):
+    os.makedirs("images")
+
 
 index_number = 193064
 e = int(str(index_number)[3])
@@ -19,20 +26,20 @@ b = VectorB(index_number=index_number, function=function)
 # B
 
 try:
-    jacobi_solver = pickle.load(open("jacobi_b.pkl", "rb"))
+    jacobi_solver = pickle.load(open("snapshots/jacobi_b.pkl", "rb"))
     print("Loaded Jacobi from file!")
 except:
     jacobi_solver = JacobiSolver(A, b)
     print("Jacobi finished!")
-    pickle.dump(jacobi_solver, open("jacobi_b.pkl", "wb"))
+    pickle.dump(jacobi_solver, open("snapshots/jacobi_b.pkl", "wb"))
 
 try:
-    gauss_solver = pickle.load(open("gauss_seidel_b.pkl", "rb"))
+    gauss_solver = pickle.load(open("snapshots/gauss_seidel_b.pkl", "rb"))
     print("Loaded Gauss-Seidel from file!")
 except:
     gauss_solver = GaussSeidelSolver(A, b)
     print("Gauss-Seidel finished!")
-    pickle.dump(gauss_solver, open("gauss_seidel_b.pkl", "wb"))
+    pickle.dump(gauss_solver, open("snapshots/gauss_seidel_b.pkl", "wb"))
 
 plt.title("Błędy w kolejnych iteracjach dla a1 = 5 + e (5), a2 = a3 = -1")
 plt.plot(gauss_solver.errors, label="Gauss-Seidel")
@@ -41,7 +48,7 @@ plt.yscale('log')
 plt.legend(["Gauss-Seidel", "Jacobi"])
 plt.ylabel("Norma wektora residuum")
 plt.xlabel("Iteracja")
-plt.savefig("bledy_zbiezne.png")
+plt.savefig("images/bledy_zbiezne.png")
 plt.close()
 
 # C 
@@ -51,20 +58,20 @@ A = UserMatrix(a1, a2, a3, index_number=index_number)
 b = VectorB(index_number=index_number, function=function)
 
 try:
-    jacobi_solver = pickle.load(open("jacobi_c.pkl", "rb"))
+    jacobi_solver = pickle.load(open("snapshots/jacobi_c.pkl", "rb"))
     print("Loaded Jacobi from file!")
 except:
     jacobi_solver = JacobiSolver(A, b)
     print("Jacobi finished!")
-    pickle.dump(jacobi_solver, open("jacobi_c.pkl", "wb"))
+    pickle.dump(jacobi_solver, open("snapshots/jacobi_c.pkl", "wb"))
 
 try:
-    gauss_solver = pickle.load(open("gauss_seidel_c.pkl", "rb"))
+    gauss_solver = pickle.load(open("snapshots/gauss_seidel_c.pkl", "rb"))
     print("Loaded Gauss-Seidel from file!")
 except:
     gauss_solver = GaussSeidelSolver(A, b)
     print("Gauss-Seidel finished!")
-    pickle.dump(gauss_solver, open("gauss_seidel_c.pkl", "wb"))
+    pickle.dump(gauss_solver, open("snapshots/gauss_seidel_c.pkl", "wb"))
 
 plt.title("Błędy w kolejnych iteracjach dla a1 = a2 = a3 = -1")
 plt.plot(gauss_solver.errors, label="Gauss-Seidel")
@@ -73,18 +80,18 @@ plt.yscale('log')
 plt.legend(["Gauss-Seidel", "Jacobi"])
 plt.ylabel("Norma wektora residuum")
 plt.xlabel("Iteracja")
-plt.savefig("bledy_rozbiezne.png")
+plt.savefig("images/bledy_rozbiezne.png")
 plt.close()
 
 # D 
 A = UserMatrix(a1, a2, a3, index_number=index_number)
 b = VectorB(index_number=index_number, function=function)
 try:
-    lu_solver = pickle.load(open("lu_d.pkl", "rb"))
+    lu_solver = pickle.load(open("snapshots/lu_d.pkl", "rb"))
     print("Loaded LU from file!")
 except:
     lu_solver = LUFactorizationSolver(A, b, debug=True)
-    pickle.dump(lu_solver, open("lu_d.pkl", "wb"))
+    pickle.dump(lu_solver, open("snapshots/lu_d.pkl", "wb"))
     print("LU finished!")
 print(f"Duration: {lu_solver.duration}")
 print(f"Errors: {lu_solver.errors}")
@@ -103,11 +110,11 @@ for size in sizes:
     b = VectorB(index_number=index_number, function=function, n=size)
 
     try:
-        jacobi_solver = pickle.load(open(f"jacobi_{size}.pkl", "rb"))
+        jacobi_solver = pickle.load(open(f"snapshots/jacobi_{size}.pkl", "rb"))
         print(f"Loaded Jacobi {size} from file!")
     except:
         jacobi_solver = JacobiSolver(A, b)
-        pickle.dump(jacobi_solver, open(f"jacobi_{size}.pkl", "wb"))
+        pickle.dump(jacobi_solver, open(f"snapshots/jacobi_{size}.pkl", "wb"))
         print(f"Jacobi {size} finished!")
     print(f"Duration: {jacobi_solver.duration}")
     print(f"Error: {jacobi_solver.errors[-1]}")
@@ -115,11 +122,11 @@ for size in sizes:
     durations_jacobi.append(jacobi_solver.duration)
     
     try:
-        gauss_seidel_solver = pickle.load(open(f"gauss_seidel_{size}.pkl", "rb"))
+        gauss_seidel_solver = pickle.load(open(f"snapshots/gauss_seidel_{size}.pkl", "rb"))
         print(f"Loaded Gauss-Seidel {size} from file!")
     except:
         gauss_seidel_solver = GaussSeidelSolver(A, b)
-        pickle.dump(gauss_seidel_solver, open(f"gauss_seidel_{size}.pkl", "wb"))
+        pickle.dump(gauss_seidel_solver, open(f"snapshots/gauss_seidel_{size}.pkl", "wb"))
         print(f"Gauss-Seidel {size} finished!")
     print(f"Duration: {gauss_seidel_solver.duration}")
     print(f"Error: {jacobi_solver.errors[-1]}")
@@ -127,11 +134,11 @@ for size in sizes:
     durations_gauss_seidel.append(gauss_seidel_solver.duration)
 
     try:
-        lu_solver = pickle.load(open(f"lu_{size}.pkl", "rb"))
+        lu_solver = pickle.load(open(f"snapshots/lu_{size}.pkl", "rb"))
         print(f"Loaded LU {size} from file!")
     except:
         lu_solver = LUFactorizationSolver(A, b)
-        pickle.dump(lu_solver, open(f"lu_{size}.pkl", "wb"))
+        pickle.dump(lu_solver, open(f"snapshots/lu_{size}.pkl", "wb"))
         print(f"LU {size} finished!")
     print(f"Duration: {lu_solver.duration}")
     print(f"Error: {lu_solver.errors[-1]}")
@@ -145,5 +152,5 @@ plt.plot(sizes, durations_lu)
 plt.legend(["Jacobi", "Gauss-Seidel", "LU"])
 plt.ylabel("Czas wykonania [s]")
 plt.xlabel("Rozmiar macierzy")
-plt.savefig("czas_wykonania.png")
+plt.savefig("images/czas_wykonania.png")
 
